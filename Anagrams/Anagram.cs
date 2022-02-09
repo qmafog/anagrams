2c6a17dbed1ff9bfd6952b3f89e8ce61c1d4be31
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-09 16:23:37 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ Anagrams/Anagram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Anagrams
+{
+    public class Anagram
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Anagram"/> class.
+        /// </summary>
+        /// <param name="sourceWord">Source word.</param>
+        /// <exception cref="ArgumentNullException">Thrown when source word is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when  source word is empty.</exception>
+        public Anagram(string? sourceWord)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+
+        /// <summary>
+        /// From the list of possible anagrams selects the correct subset.
+        /// </summary>
+        /// <param name="candidates">A list of possible anagrams.</param>
+        /// <returns>The correct sublist of anagrams.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when candidates list is null.</exception>
+        public string[] FindAnagrams(string[]? candidates)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
